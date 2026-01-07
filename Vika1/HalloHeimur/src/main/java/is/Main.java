12e71734af7f8526a5cbf68225df9a383d114951
Ebba Þóra Hvannberg <ebba@hi.is>
2026-01-07 20:53:10 +0000

    Halló heimur í Java

---
+++ Vika1/HalloHeimur/src/main/java/is/Main.java
@@ -0,0 +1,23 @@
+package is;
+
+/******************************************************************************
+ *  Nafn    : Ebba Þóra Hvannberg
+ *  T-póstur: ebba@hi.is
+ *  Lýsing  : Dæmi um einfalt Halló heimur forrit í Java sem keyrir með Maven í IntelliJ
+ *
+ *
+ *****************************************************************************///TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        IO.println(String.format("Halló - þetta er venjulegt Java forrit keyrt með Maven "));
+
+        for (int i = 1; i <= 5; i++) {
+            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+            IO.println("i = " + i);
+        }
+    }
+}
