d48c2eed5a9d065ac4a507093c3179351a960455
Ebba Þóra Hvannberg <ebba@hi.is>
2026-01-11 18:08:28 +0000

    Fyrsta útgáfa

---
+++ Vika2/SamaBreidd/src/main/java/vidmot/Main.java
@@ -0,0 +1,26 @@
+package vidmot;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("vidmot.fxml"));
+        primaryStage.setTitle("Sama breidd");
+        // Ef þú vilt ekki að notandi stækki eða minnki gluggann
+
+      //  primaryStage.setResizable (false);
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
